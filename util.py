940859d17b2d6aacbabeb36c05c64c5552806590
sgg2022 <sgg2022.sg@gmail.com>
2023-03-27 00:28:22 +0200

    Completada la función que selecciona los parametros del juego

--- util.py
+++ util.py
@@ -23,3 +23,24 @@ def traducir_texto(texto: str, idioma_origen: str, idioma_destino: str) -> str:
 
 # Ejemplo idiomas
 
+def seleccionSala(categoria, idioma,dif,tipo):
+    datoscategorias = ['Any Category','General Knowledge','Books','Film','Music','Musicals & Theatres','Television','Videogames','Board Games','Science & Nature','Computers','Mathematics','Mythology','Sports','Geography','History','Politics','Art','Celebrities','Animals','Vehicles','Comics','Gadgets','Japanese Anime & Manga','Cartoon & Animations']
+    datosIdioma = ['af', 'sq', 'am', 'ar', 'hy', 'az', 'eu', 'be', 'bn', 'bs', 'bg', 'ca', 'ceb', 'ny', 'zh-cn', 'zh-tw', 'co', 'hr', 'cs', 'da', 'nl', 'en', 'eo', 'et', 'tl', 'fi', 'fr', 'fy', 'gl', 'ka', 'de', 'el', 'gu', 'ht', 'ha', 'haw', 'he', 'iw', 'hi', 'hmn', 'hu', 'is', 'ig', 'id', 'in', 'ga', 'it', 'ja', 'jw', 'kn', 'kk', 'km', 'rw', 'ko', 'ku', 'ky', 'lo', 'la', 'lv', 'lt', 'lb', 'mk', 'mg', 'ms', 'ml', 'mt', 'mi', 'mr', 'mn', 'my', 'ne', 'no', 'ny', 'ps', 'fa', 'pl', 'pt', 'pa', 'ro', 'ru', 'sm', 'gd', 'sr', 'st', 'sn', 'sd', 'si', 'sk', 'sl', 'so', 'es', 'su', 'sw', 'sv', 'tg', 'ta', 'te', 'th', 'tr', 'uk', 'ur']
+    dificultad = ['Any Difficulty','Easy','Medium','Hard']
+    type = ['Any Type','Multiple Choice','True/False']
+    for cat_aux in datoscategorias:
+        for idi_aux in datosIdioma:
+            for dif_aux in dificultad:
+                for typ_aux in type:
+                    if categoria == cat_aux and idioma == idi_aux and dif == dif_aux and tipo == typ_aux:
+                     return True
+
+    return False
+
+
+resultado = seleccionSala('Art','ar','Hard','True/False')
+print(resultado)
+
+
+
+
